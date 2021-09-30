e1952c8de70545cfd2266d6e8bdff276929743bd
Maxxxxxon <m.faleev@innopolis.university>
2021-09-30 18:57:17 +0300

    - django table for habits

--- biproductive/habits/urls.py
+++ biproductive/habits/urls.py
@@ -5,5 +5,4 @@ from . import views
 app_name = 'habits'
 urlpatterns = [
     path('', views.IndexView.as_view()),
-    path('tables/', views.last_week_habit_usage)
 ]
--- biproductive/habits/views.py
+++ biproductive/habits/views.py
@@ -12,8 +12,3 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Habit.objects.all()
 
-
-def last_week_habit_usage(request):
-    data = load_last_week_habit_usage()
-    table = LastWeekHabitUsageTable(data)
-    return render(request, 'habits/habit_usage_table.html', {"table": table})
