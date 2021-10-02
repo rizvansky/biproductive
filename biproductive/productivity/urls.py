bb9888c8fa2b1240d5d36cdaa377354ab5dd595a
s.arslanov <s.arslanov@innopolis.university>
2021-10-02 15:14:24 +0300

    Added fetch query for setting results of game to django database. Added javascript for playing the game Wrote test for productivity_view Changed logic of handling post request in productivity view for working with javascript library `filter`

--- biproductive/productivity/urls.py
+++ biproductive/productivity/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from . import views
 
-app_name = 'productivity'
 urlpatterns = [
-    path('', views.productivity),
+    path("", views.productivity, name="game"),
 ]
