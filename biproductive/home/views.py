4d2b66fda4a07fe5da917fd86dabf77d637f6a02
s.arslanov <s.arslanov@innopolis.university>
2021-10-04 23:31:15 +0300

    render frontend home page

--- biproductive/home/views.py
+++ biproductive/home/views.py
@@ -1,9 +1,47 @@
+import json
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+data = [
+    {
+        "date": "04.10.2021",
+        "Drinking tea": "yes",
+        "Smoking": "no",
+        "Making startup": "yes",
+        "Drinking beer": "yes",
+    },
+    {
+        "date": "05.10.2021",
+        "Drinking tea": "no",
+        "Smoking": "yes",
+        "Making startup": "yes",
+        "Drinking beer": "yes",
+    },
+]
 
 
 @login_required(login_url="login")
 def home_view(request):
-    return render(request=request, template_name="home.html", context={})
+    return render(
+        request=request,
+        template_name="home.html",
+        context={
+            "brain_chart": json.dumps(
+                {
+                    "date": [
+                        "04.10.2021",
+                        "05.10.2021",
+                        "06.10.2021",
+                        "07.10.2021",
+                        "08.10.2021",
+                        "09.10.2021",
+                    ],
+                    "brain-activity": [10, 20, 25, 30, 60, 50],
+                }
+            ),
+            "habit_table_data": json.dumps(data),
+            "habit_names": list(data[0].keys()),
+        },
+    )
