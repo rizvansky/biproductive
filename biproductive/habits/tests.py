d36330cda96e0e49b2c170317ad13e2be8daeead
s.arslanov <s.arslanov@innopolis.university>
2021-10-04 21:02:57 +0300

    bugfix: incorrect reverse access'

--- biproductive/habits/tests.py
+++ biproductive/habits/tests.py
@@ -1,11 +1,8 @@
 import json
-from datetime import date
-from http import HTTPStatus
 
 from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import reverse
-from habits.models import ProductivityCheck
 
 
 class TestProductivityGame(TestCase):
@@ -24,14 +21,25 @@ class TestProductivityGame(TestCase):
             self.client.login(username=self.username, password=self.password),
             "login failed",
         )
-        response = self.client.get(path=reverse("add_habit"), data={'habit_name': 'test_habit'}),
+        response = (
+            self.client.get(
+                path=reverse("habits:add_habit"), data={"habit_name": "test_habit"}
+            ),
+        )
         self.assertEqual(response.code, 200)
-        response = self.client.get(path=reverse("add_habit"), data={'habit_name': 'test_habit2'}),
+        response = (
+            self.client.get(
+                path=reverse("habits:add_habit"), data={"habit_name": "test_habit2"}
+            ),
+        )
         self.assertEqual(response.code, 200)
         table = self.client.get(path=reverse("habit_table"), data={})
-        correct_table = json.dumps({
-            'table': {
-                'test_habit1': ['-', '-', '-', '-', '-', '-', '-'],
-                'test_habit2': ['-', '-', '-', '-', '-', '-', '-'],
-            }})
+        correct_table = json.dumps(
+            {
+                "table": {
+                    "test_habit1": ["-", "-", "-", "-", "-", "-", "-"],
+                    "test_habit2": ["-", "-", "-", "-", "-", "-", "-"],
+                }
+            }
+        )
         self.assertEqual(table, correct_table)
